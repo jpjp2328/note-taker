e2cd50f4ffa1c54f217ab7c17be289e898a7c004
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-10-30 23:32:57 +1100

    Api GET/POST routes added, Tested

--- server.js
+++ server.js
@@ -25,9 +25,33 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))  
 );
 
-// GET Route for retrieving all the notes
+// GET Route for retrieving all the notes as JSON
+app.get('/api/notes', (req, res) => {
+    console.log(`${req.method} request received for Notes`);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw(err);
+        res.json(JSON.parse(data));
+    });
+}); 
 
 // POST Route 
+app.post('/api/notes', (req, res) => {
+    console.log(`${req.method} request received to add a Note`);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+
+        let notes = JSON.parse(data);
+        let newNote = req.body;
+        
+        newNote.id = uuid();
+        notes.push(newNote);
+        
+        fs.writeFile('./db/db.json', JSON.stringify(notes), 'utf8', (err) => {
+            if (err) throw err;
+            console.log('Updated Notes');
+        });
+    });
+});
 
 app.listen(PORT, () => 
   console.log(`App listening at http://localhost:${PORT}`)
